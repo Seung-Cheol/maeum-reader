bc759acd2a2d729022f9b1f96cef2167b3de8df1
Seung-Cheol <rlatmdcjf10@naver.com>
2023-12-15 00:09:44 +0900

    asdad

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log(process.env.DB_HOST+"1")
   const app = await NestFactory.create(AppModule);
   app.enableCors({ 
     origin: [ 
